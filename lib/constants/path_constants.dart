15e0468d07fd25bc1aab5a63a865c071e35ea124
StefanoChiodini <stefano.chiodini@mail.polimi.it>
2023-05-10 17:24:37 +0200

    started weather screens

--- lib/constants/path_constants.dart
+++ lib/constants/path_constants.dart
@@ -3,4 +3,5 @@ class PathConstants {
   static const String onboarding1 = 'lib/assets/images/skiing_1.png';
   static const String onboarding2 = 'lib/assets/images/skiing_2.png';
   static const String onboarding3 = 'lib/assets/images/skiing_2.png';
+  static const String sunnyDay = 'lib/assets/images/sunnyDay.jpg';
 }
---
+++ lib/screens/meteo/meteo_screen.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:ski_resorts_app/constants/path_constants.dart';
+
+enum SearchMethod {
+  manual,
+  automatic,
+}
+
+class MeteoPageScreen extends StatefulWidget {
+  const MeteoPageScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MeteoPageScreen> createState() => _MeteoPageScreenState();
+}
+
+class _MeteoPageScreenState extends State<MeteoPageScreen> {
+  final TextEditingController _cityController = TextEditingController();
+  SearchMethod _searchMethod = SearchMethod.manual;
+
+  void _submitCity(BuildContext context) {
+    final enteredCity = _cityController.text;
+    // TODO: Implement weather data fetching based on the entered city
+    print('Submitted city: $enteredCity');
+  }
+
+  void _selectSearchMethod(SearchMethod method) {
+    setState(() {
+      _searchMethod = method;
+      if (method == SearchMethod.automatic) {
+        // TODO: Implement automatic location detection
+        print('Using current location');
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _cityController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(PathConstants.sunnyDay),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Stack(
+                  children: [
+                    Opacity(
+                      opacity: 0.7,
+                      child: Container(
+                        height: 40,
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Text(
+                          'Enter a location',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            controller: _cityController,
+                            decoration: const InputDecoration(
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                        PopupMenuButton<SearchMethod>(
+                          initialValue: _searchMethod,
+                          onSelected: (SearchMethod method) {
+                            _selectSearchMethod(method);
+                          },
+                          itemBuilder: (BuildContext context) =>
+                              <PopupMenuEntry<SearchMethod>>[
+                            const PopupMenuItem<SearchMethod>(
+                              value: SearchMethod.manual,
+                              child: ListTile(
+                                leading: Icon(Icons.location_on),
+                                title: Text('Enter city manually'),
+                              ),
+                            ),
+                            const PopupMenuItem<SearchMethod>(
+                              value: SearchMethod.automatic,
+                              child: ListTile(
+                                leading: Icon(Icons.my_location),
+                                title: Text('Use current location'),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => _submitCity(context),
+                child: const Text('Search'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
