bfe606efcb91cbff8ea0b86ccf0e06c3c215a442
FrancescoZanella <francescozanella00@gmail.com>
2023-07-19 11:47:36 +0200

    reordered

--- lib/screens/loginOrRegistration/login/login_screen.dart
+++ lib/screens/loginOrRegistration/login/login_screen.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/gestures.dart';
+import 'package:ski_resorts_app/screens/loginOrRegistration/login/data_login.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:ski_resorts_app/screens/loginOrRegistration/login_screen.dart';
+import 'package:ski_resorts_app/screens/loginOrRegistration/registration/registration_screen.dart';
 
-class RegistrationPage extends StatefulWidget {
-  const RegistrationPage({Key? key}) : super(key: key);
+import '../APIloginAndSignin/facebook_auth.dart';
+import '../APIloginAndSignin/google_auth.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
-  State<RegistrationPage> createState() => _RegistrationPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _RegistrationPageState extends State<RegistrationPage>
+class _LoginPageState extends State<LoginPage>
     with SingleTickerProviderStateMixin {
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _surnameController = TextEditingController();
-  final TextEditingController _phoneNumberController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   bool obscure = true;
-
   void _viewPassword() {
     setState(() {
       obscure = !obscure;
@@ -29,9 +29,6 @@ class _RegistrationPageState extends State<RegistrationPage>
   void dispose() {
     _emailController.dispose();
     _passwordController.dispose();
-    _nameController.dispose();
-    _surnameController.dispose();
-    _phoneNumberController.dispose();
     super.dispose();
   }
 
@@ -43,17 +40,8 @@ class _RegistrationPageState extends State<RegistrationPage>
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.black,
-      body: buildCard(
-          context,
-          size,
-          _emailController,
-          _passwordController,
-          _nameController,
-          _surnameController,
-          _phoneNumberController,
-          isPressed,
-          _viewPassword,
-          obscure),
+      body: buildCard(context, size, _emailController, _passwordController,
+          isPressed, _viewPassword, obscure),
     );
   }
 }
@@ -63,9 +51,6 @@ Widget buildCard(
     Size size,
     TextEditingController emailController,
     TextEditingController passwordController,
-    TextEditingController nameController,
-    TextEditingController surnameController,
-    TextEditingController phoneNumberController,
     bool isPressed,
     var callback,
     bool obscure) {
@@ -83,12 +68,12 @@ Widget buildCard(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           SizedBox(
-            height: size.height * 0.03,
+            height: size.height * 0.1,
           ),
 
           //welcome text
           Text(
-            'Create an Account!',
+            'Welcome Back!',
             style: GoogleFonts.inter(
               fontSize: 22.0,
               color: Colors.black,
@@ -99,7 +84,7 @@ Widget buildCard(
             height: size.height * 0.01,
           ),
           Text(
-            'Let’s create an account to have fun skiing',
+            'Let’s login for explore your skiing progress',
             style: GoogleFonts.inter(
               fontSize: 14.0,
               color: const Color(0xFF969AA8),
@@ -108,66 +93,20 @@ Widget buildCard(
           SizedBox(
             height: size.height * 0.03,
           ),
+
           //logo section
           logo(size.height / 12, size.height / 12),
           SizedBox(
             height: size.height * 0.02,
           ),
-          richText(22),
+          richText(24),
           SizedBox(
-            height: size.height * 0.01,
+            height: size.height * 0.03,
           ),
-          //name textField
-          Text(
-            'Name',
-            style: GoogleFonts.inter(
-              fontSize: 14.0,
-              color: Colors.black,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          SizedBox(
-            height: size.height * 0.01,
-          ),
-          nameTextField(size, nameController),
-          SizedBox(
-            height: size.height * 0.02,
-          ),
-          //surname textField
-          Text(
-            'Surname',
-            style: GoogleFonts.inter(
-              fontSize: 14.0,
-              color: Colors.black,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          SizedBox(
-            height: size.height * 0.01,
-          ),
-          surnameTextField(size, surnameController),
-          SizedBox(
-            height: size.height * 0.02,
-          ),
-          //phone textField
-          Text(
-            'Phone number',
-            style: GoogleFonts.inter(
-              fontSize: 14.0,
-              color: Colors.black,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          SizedBox(
-            height: size.height * 0.01,
-          ),
-          phoneTextField(size, phoneNumberController),
-          SizedBox(
-            height: size.height * 0.02,
-          ),
+
           //email textField
           Text(
-            'Email',
+            'Email or Phone Number',
             style: GoogleFonts.inter(
               fontSize: 14.0,
               color: Colors.black,
@@ -181,6 +120,7 @@ Widget buildCard(
           SizedBox(
             height: size.height * 0.02,
           ),
+
           //password textField
           Text(
             'Password',
@@ -195,15 +135,40 @@ Widget buildCard(
           ),
           passwordTextField(size, passwordController, callback, obscure),
           SizedBox(
-            height: size.height * 0.02,
+            height: size.height * 0.03,
           ),
 
           //sign in button
-          signUpButton(context, size, isPressed, callback, emailController.text,
+          signInButton(context, size, isPressed, callback, emailController.text,
               passwordController.text),
           SizedBox(
-            height: size.height * 0.02,
+            height: size.height * 0.03,
           ),
+
+          //we can connect text
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Expanded(child: Divider()),
+              const SizedBox(
+                width: 16,
+              ),
+              Text(
+                'We can Connect with',
+                style: GoogleFonts.inter(
+                  fontSize: 12.0,
+                  color: const Color(0xFF969AA8),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              const Expanded(child: Divider()),
+            ],
+          ),
+
           //footer section
           buildFooter(size, context),
         ],
@@ -230,7 +195,7 @@ Widget richText(double fontSize) {
       ),
       children: const [
         TextSpan(
-          text: 'SIGN-UP',
+          text: 'SIGN-IN',
           style: TextStyle(
             fontWeight: FontWeight.w800,
           ),
@@ -240,195 +205,9 @@ Widget richText(double fontSize) {
   );
 }
 
-Widget nameTextField(Size size, TextEditingController nameController) {
-  return SizedBox(
-    height: size.height / 15,
-    child: TextField(
-      controller: nameController,
-      style: GoogleFonts.inter(
-        fontSize: 18.0,
-        color: const Color(0xFF151624),
-      ),
-      maxLines: 1,
-      keyboardType: TextInputType.name,
-      cursorColor: const Color(0xFF151624),
-      decoration: InputDecoration(
-        hintText: 'Enter your name',
-        hintStyle: GoogleFonts.inter(
-          fontSize: 16.0,
-          color: const Color(0xFF151624).withOpacity(0.5),
-        ),
-        filled: true,
-        fillColor: nameController.text.isEmpty
-            ? const Color.fromRGBO(248, 247, 251, 1)
-            : Colors.transparent,
-        enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
-            borderSide: BorderSide(
-              color: nameController.text.isEmpty
-                  ? Colors.transparent
-                  : const Color.fromRGBO(44, 185, 176, 1),
-            )),
-        focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
-            borderSide: const BorderSide(
-              color: Color.fromRGBO(44, 185, 176, 1),
-            )),
-        prefixIcon: Icon(
-          Icons.assignment_ind,
-          color: nameController.text.isEmpty
-              ? const Color(0xFF151624).withOpacity(0.5)
-              : const Color.fromRGBO(44, 185, 176, 1),
-          size: 16,
-        ),
-        suffix: Container(
-          alignment: Alignment.center,
-          width: 24.0,
-          height: 24.0,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(100.0),
-            color: const Color.fromRGBO(44, 185, 176, 1),
-          ),
-          child: nameController.text.isEmpty
-              ? const Center()
-              : const Icon(
-                  Icons.check,
-                  color: Colors.white,
-                  size: 13,
-                ),
-        ),
-      ),
-    ),
-  );
-}
-
-Widget surnameTextField(Size size, TextEditingController surnameController) {
-  return SizedBox(
-    height: size.height / 15,
-    child: TextField(
-      controller: surnameController,
-      style: GoogleFonts.inter(
-        fontSize: 18.0,
-        color: const Color(0xFF151624),
-      ),
-      maxLines: 1,
-      keyboardType: TextInputType.name,
-      cursorColor: const Color(0xFF151624),
-      decoration: InputDecoration(
-        hintText: 'Enter your surname',
-        hintStyle: GoogleFonts.inter(
-          fontSize: 16.0,
-          color: const Color(0xFF151624).withOpacity(0.5),
-        ),
-        filled: true,
-        fillColor: surnameController.text.isEmpty
-            ? const Color.fromRGBO(248, 247, 251, 1)
-            : Colors.transparent,
-        enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
-            borderSide: BorderSide(
-              color: surnameController.text.isEmpty
-                  ? Colors.transparent
-                  : const Color.fromRGBO(44, 185, 176, 1),
-            )),
-        focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
-            borderSide: const BorderSide(
-              color: Color.fromRGBO(44, 185, 176, 1),
-            )),
-        prefixIcon: Icon(
-          Icons.assignment_ind,
-          color: surnameController.text.isEmpty
-              ? const Color(0xFF151624).withOpacity(0.5)
-              : const Color.fromRGBO(44, 185, 176, 1),
-          size: 16,
-        ),
-        suffix: Container(
-          alignment: Alignment.center,
-          width: 24.0,
-          height: 24.0,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(100.0),
-            color: const Color.fromRGBO(44, 185, 176, 1),
-          ),
-          child: surnameController.text.isEmpty
-              ? const Center()
-              : const Icon(
-                  Icons.check,
-                  color: Colors.white,
-                  size: 13,
-                ),
-        ),
-      ),
-    ),
-  );
-}
-
-Widget phoneTextField(Size size, TextEditingController phoneNumberController) {
-  return SizedBox(
-    height: size.height / 15,
-    child: TextField(
-      controller: phoneNumberController,
-      style: GoogleFonts.inter(
-        fontSize: 18.0,
-        color: const Color(0xFF151624),
-      ),
-      maxLines: 1,
-      keyboardType: TextInputType.name,
-      cursorColor: const Color(0xFF151624),
-      decoration: InputDecoration(
-        hintText: 'Enter your phone number',
-        hintStyle: GoogleFonts.inter(
-          fontSize: 16.0,
-          color: const Color(0xFF151624).withOpacity(0.5),
-        ),
-        filled: true,
-        fillColor: phoneNumberController.text.isEmpty
-            ? const Color.fromRGBO(248, 247, 251, 1)
-            : Colors.transparent,
-        enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
-            borderSide: BorderSide(
-              color: phoneNumberController.text.isEmpty
-                  ? Colors.transparent
-                  : const Color.fromRGBO(44, 185, 176, 1),
-            )),
-        focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
-            borderSide: const BorderSide(
-              color: Color.fromRGBO(44, 185, 176, 1),
-            )),
-        prefixIcon: Icon(
-          Icons.phone_android,
-          color: phoneNumberController.text.isEmpty
-              ? const Color(0xFF151624).withOpacity(0.5)
-              : const Color.fromRGBO(44, 185, 176, 1),
-          size: 16,
-        ),
-        suffix: Container(
-          alignment: Alignment.center,
-          width: 24.0,
-          height: 24.0,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(100.0),
-            color: const Color.fromRGBO(44, 185, 176, 1),
-          ),
-          child: phoneNumberController.text.isEmpty
-              ? const Center()
-              : const Icon(
-                  Icons.check,
-                  color: Colors.white,
-                  size: 13,
-                ),
-        ),
-      ),
-    ),
-  );
-}
-
 Widget emailTextField(Size size, TextEditingController emailController) {
   return SizedBox(
-    height: size.height / 15,
+    height: size.height / 13,
     child: TextField(
       controller: emailController,
       style: GoogleFonts.inter(
@@ -491,7 +270,7 @@ Widget emailTextField(Size size, TextEditingController emailController) {
 Widget passwordTextField(Size size, TextEditingController passController,
     var callback, bool obscure) {
   return Container(
-    height: size.height / 15,
+    height: size.height / 13,
     decoration: BoxDecoration(
       borderRadius: BorderRadius.circular(8),
       color: const Color.fromRGBO(248, 247, 251, 1),
@@ -565,7 +344,7 @@ Widget passwordTextField(Size size, TextEditingController passController,
   );
 }
 
-Widget signUpButton(BuildContext context, size, bool isPressed, var callback,
+Widget signInButton(BuildContext context, size, bool isPressed, var callback,
     String email, String password) {
   return Container(
       decoration: BoxDecoration(
@@ -583,20 +362,15 @@ Widget signUpButton(BuildContext context, size, bool isPressed, var callback,
           color: Colors.transparent,
           child: InkWell(
               onTap: () async {
-                /* QUELLO CHE FA QUANDO VIENE SCHIACCIATO IL SIGN UP*/
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const LoginPage(),
-                  ), // Redirect to home page
-                );
+                /* QUELLO CHE FA QUANDO VIENE SCHIACCIATO IL LOGIN*/
+                checkCredentials(context, email, password);
                 /*QUESTA PARTE VA CORRETTA*/
               },
               child: Container(
                 alignment: Alignment.center,
-                height: size.height / 15,
+                height: size.height / 13,
                 child: Text(
-                  'Sign up',
+                  'Sign in',
                   style: GoogleFonts.inter(
                     fontSize: 16.0,
                     color: Colors.white,
@@ -612,7 +386,72 @@ Widget buildFooter(Size size, BuildContext context) {
   return Center(
     child: Column(
       children: <Widget>[
+        //social logo: facebook, google
+        SizedBox(
+          height: size.height * 0.05,
+        ),
+        SizedBox(
+          width: size.width * 0.6,
+          height: 44.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              //google logo here
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25.0),
+                    color: const Color.fromRGBO(246, 246, 246, 1)),
+                child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      onTap: () async {},
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: 44.0,
+                        height: 44.0,
+                        child: IconButton(
+                          icon: Image.asset(
+                            'lib/assets/logo/google_logo.svg.png',
+                          ),
+                          iconSize: 23,
+                          onPressed: () => {signInWithGoogle(context)},
+                        ),
+                      ),
+                    )),
+              ),
+              const SizedBox(width: 16),
+              //facebook logo here
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25.0),
+                    color: const Color.fromRGBO(246, 246, 246, 1)),
+                child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      onTap: () async {},
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: 44.0,
+                        height: 44.0,
+                        child: IconButton(
+                          icon: Image.asset(
+                            'lib/assets/logo/facebook_logo.png',
+                          ),
+                          iconSize: 23,
+                          onPressed: () =>
+                              {onFacebookLoginButtonPressed(context)},
+                        ),
+                      ),
+                    )),
+              ),
+            ],
+          ),
+        ),
+
         //footer text 'sign up' text here
+        SizedBox(
+          height: size.height * 0.03,
+        ),
         Text.rich(
           TextSpan(
             style: GoogleFonts.inter(
@@ -621,13 +460,13 @@ Widget buildFooter(Size size, BuildContext context) {
             ),
             children: [
               const TextSpan(
-                text: 'Already have an account? ',
+                text: 'Don’t have an account? ',
                 style: TextStyle(
                   fontWeight: FontWeight.w500,
                 ),
               ),
               TextSpan(
-                  text: 'Sign in here',
+                  text: 'Sign Up here',
                   style: const TextStyle(
                     color: Color(0xFFFF7248),
                     fontWeight: FontWeight.w500,
@@ -635,11 +474,10 @@ Widget buildFooter(Size size, BuildContext context) {
                   recognizer: TapGestureRecognizer()
                     ..onTap = () {
                       Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const LoginPage(),
-                        ), // Redirect to home page
-                      );
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const RegistrationPage(),
+                          ));
                     }),
             ],
           ),
