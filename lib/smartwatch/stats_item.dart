ed087c40ca467898a7f2599f3234eda4e978c1af
FrancescoZanella <francescozanella00@gmail.com>
2023-08-27 14:28:39 +0200

    fixed stats

--- lib/smartwatch/stats_item.dart
+++ lib/smartwatch/stats_item.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:ski_resorts_app/smartwatch/overwiew_stats.dart';
 
 import '../phone/screens/statistics/statistics_screen_1.dart';
 
 class StatsItem extends StatelessWidget {
-  const StatsItem({required this.data});
+  const StatsItem({super.key, required this.data});
 
   final RunData data;
 
@@ -12,43 +12,54 @@ class StatsItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Card(
-        color: Colors.black,
-        child: Row(
-          children: [
-            const SizedBox(
-              width: 15,
-            ),
-            Container(
-              width: 35,
-              height: 35,
-              decoration: const BoxDecoration(
-                color: Colors.blueAccent,
-                shape: BoxShape.circle,
+      child: GestureDetector(
+        child: Card(
+          color: Colors.black,
+          child: Row(
+            children: [
+              const SizedBox(
+                width: 15,
               ),
-              child: Image.asset(
-                'lib/assets/icons/output.png',
-                height: 22, // Adjust the image size as needed
-              ),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Column(
-              children: [
-                Text(
-                  "${data.distanceInMeters.toStringAsFixed(2)} km",
-                  style: TextStyle(color: Colors.white),
+              Container(
+                width: 35,
+                height: 35,
+                decoration: const BoxDecoration(
+                  color: Colors.blueAccent,
+                  shape: BoxShape.circle,
                 ),
-                Text(data.date, style: TextStyle(color: Colors.white)),
-              ],
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Icon(Icons.arrow_forward_ios_rounded, color: Colors.white),
-          ],
+                child: Image.asset(
+                  'lib/assets/icons/output.png',
+                  height: 22, // Adjust the image size as needed
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Column(
+                children: [
+                  Text(
+                    "${data.distanceInMeters.toStringAsFixed(2)} m",
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  Text(data.date, style: const TextStyle(color: Colors.white)),
+                ],
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              const Icon(Icons.arrow_forward_ios_rounded, color: Colors.white),
+            ],
+          ),
         ),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => Overview(
+                      data: data,
+                    )),
+          );
+        },
       ),
     );
   }
